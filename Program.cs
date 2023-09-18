146550d4f37b0ca14267ab70dea55af3bd543438
ashley ingram <ashleyingram.437@gmail.com>
2023-09-17 20:08:50 -0400

    I did it!

--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<DogFood>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myClass = new DogFood();
+
+                Console.WriteLine("Enter the value for ");
+                Dryorganic = Console.ReadLine();
+                Dogleash = Console.ReadLine();
+                quanity = Console.ReadLine();
+                price = Console.ReadLine();
+                
+
+
+
+                recordList.Add(myClass);
+            }
+
+            // Print out the list of records using Console.WriteLine()
+
         }
     }
-}
